3c8ace97300771a96f62d7b3faa8ceb22975b906
Ada <the.nostra.tymus@gmail.com>
2025-02-17 20:07:55 -0500

    updated the crypto layer of the it accounts manager

--- simulating_software/it_account_simulator/it_account_simulator/app.py
+++ simulating_software/it_account_simulator/it_account_simulator/app.py
@@ -1,17 +1,25 @@
 from flask import Flask, request, Response
 import json
-
+import crypto_layer as cl
 import utilities as u
 
 app = Flask(__name__)
 
 # Initialize Database
 u.init_db()
+crypto = cl.CryptoLayer()
+
+
+@app.route('/key_id', methods=['GET'])
+def get_key_id():
+    return Response(json.dumps({'key_id': crypto.key_id}), status=200, mimetype='application/json')
 
 
 @app.route('/accounts', methods=['GET'])
 def get_accounts():
     """Fetch all accounts from the database."""
+    sender_key = request.args.get('sender_key')
+
     conn = u.get_db_connection()
     cursor = conn.cursor()
 
@@ -21,7 +29,7 @@ def get_accounts():
     cursor.close()
     conn.close()
 
-    return Response(json.dumps(accounts), status=200, mimetype='application/json')
+    return Response(crypto.create_payload(accounts, sender_key), status=200, mimetype='application/json')
 
 
 @app.route('/register', methods=['POST'])
@@ -31,6 +39,8 @@ def register_account():
         return Response("Invalid Request", status=400, mimetype='application/json')
 
     data = json.loads(request.json)
+    sender_key = data["sender_key"]
+    data = crypto.decrypt_message(data)
 
     first_name = data['first_name']
     last_name = data['last_name']
@@ -38,7 +48,8 @@ def register_account():
     key_id = data['key_id']
     account_name = u.add_user_account(first_name, last_name, employee_number, key_id)
 
-    return Response(json.dumps({'account': account_name}), status=200, mimetype='application/json')
+    return Response(crypto.create_payload({'account': account_name}, sender_key), status=200,
+                    mimetype='application/json')
 
 
 if __name__ == '__main__':
